8c90263b5eced6c0768cddb59a3a934e593a8136
Quinn Slack <qslack@qslack.com>
2015-05-04 01:48:30 -0700

    make Timestamp JSON-(un)marshalable

--- timestamp_test.go
+++ timestamp_test.go
@@ -1,6 +1,7 @@
 package pbtypes
 
 import (
+	"encoding/json"
 	"testing"
 	"time"
 )
@@ -11,5 +12,22 @@ func TestTimestamp(t *testing.T) {
 	tm2 := ts.Time()
 	if !tm2.Equal(tm) {
 		t.Errorf("got %q, want %q", tm2, tm)
+	}
+}
+
+func TestTimestamp_JSON(t *testing.T) {
+	ts := NewTimestamp(time.Now())
+	b, err := json.Marshal(ts)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	var ts2 Timestamp
+	if err := json.Unmarshal(b, &ts2); err != nil {
+		t.Fatal(err)
+	}
+
+	if ts2 != ts {
+		t.Errorf("got %v, want %v", ts2, ts)
 	}
 }
